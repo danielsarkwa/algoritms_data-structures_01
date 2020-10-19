dcce646300722f597b7272f481804d2d35db7450
DanielSarkwa <dannysark16@gmail.com>
2020-10-19 11:49:59 -0700

    completed the singly linked list and started dll

---
+++ Data structures/doubly_linked_list/doubly_linked_list.js
@@ -0,0 +1,15 @@
+class Node {
+    constructor() {
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+}
