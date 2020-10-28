9b92509a806683d5a60a6f5e823b54579950dfcf
DanielSarkwa <dannysark16@gmail.com>
2020-10-28 09:57:32 -0700

    completed dijkra's algorithms

--- Data structures/graphs/graphs.js
+++ Data structures/graphs/graphs.js
@@ -5,33 +5,21 @@
     // unweighted graphs
 
 // undirected graph
-class Graph {
-    constructor() {
-        this.adjacencyList = {}
+class Graph{
+    constructor(){
+        this.adjacencyList = {};
     }
 
-    addVertex(vertex) {
-        if( this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    addVertex(vertex){
+        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
     }
 
-    addEdge(vertex1, vertex2) {
+    addEdge(vertex1,vertex2){
         this.adjacencyList[vertex1].push(vertex2);
         this.adjacencyList[vertex2].push(vertex1);
     }
 
-    removeVertex(vertex) {
-        while(this.adjacencyList[vertex].length) {
-            const adjacencyVertex = this.adjacencyList[vertex].pop();
-            this.removeEdge(vertex, adjacencyVertex);
-        };
-
-        delete this.adjacencyList[vertex];
-    }
-
-    removeEdge(vertex1, vertex2) {
-        // return every vertex, except the one we want to remove
-        
-        // remove vertex2 from vertex1 edges
+    removeEdge(vertex1,vertex2){
         this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
             v => v !== vertex2
         );
@@ -39,9 +27,107 @@ class Graph {
             v => v !== vertex1
         );
     }
+
+    removeVertex(vertex){
+        // return every vertex, except the one we want to remove
+        
+        // remove vertex2 from vertex1 edges
+        while(this.adjacencyList[vertex].length){
+            const adjacentVertex = this.adjacencyList[vertex].pop();
+            this.removeEdge(vertex, adjacentVertex);
+        }
+        delete this.adjacencyList[vertex]
+    }
+
+    depthFirstRecursive(start){
+        const result = [];
+        const visited = {};
+        const adjacencyList = this.adjacencyList;
+
+        // perform the search
+        (function dfs(vertex){
+            if(!vertex) return null;
+            visited[vertex] = true;
+            result.push(vertex);
+
+            // loop over the vertex's children to perform search on them
+            adjacencyList[vertex].forEach(neighbor => {
+                if(!visited[neighbor]){
+                    return dfs(neighbor)
+                }
+            });
+        })(start);
+
+        return result;
+    }
+
+    depthFirstIterative(start){
+        const stack = [start];
+        const result = [];
+        const visited = {};
+        let currentVertex;
+
+        visited[start] = true;
+
+        // continue searching if the stack has items
+        while(stack.length){
+            currentVertex = stack.pop();
+            result.push(currentVertex);
+
+            // loop over the currentVertex's children -- if visited ignore, act on it
+            this.adjacencyList[currentVertex].forEach(neighbor => {
+               if(!visited[neighbor]){
+                   visited[neighbor] = true;
+                   stack.push(neighbor)
+               } 
+            });
+        }
+        return result;
+    }
+
+    breadthFirst(start){
+        const queue = [start];
+        const result = [];
+        const visited = {};
+        let currentVertex;
+        visited[start] = true;
+
+        while(queue.length){
+            currentVertex = queue.shift();
+            result.push(currentVertex);
+           
+
+            this.adjacencyList[currentVertex].forEach(neighbor => {
+                if(!visited[neighbor]){
+                    visited[neighbor] = true;
+                    queue.push(neighbor);
+                }
+            });
+        }
+        return result;
+    }
 }
 
-var g = new Graph();
-g.addVertex("Accra");
-g.addVertex("Kumasi");
-g.addVertex("Aspen");
+
+
+let g = new Graph();
+
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
+
+
+g.addEdge("A", "B");
+g.addEdge("A", "C");
+g.addEdge("B","D");
+g.addEdge("C","E");
+g.addEdge("D","E");
+g.addEdge("D","F");
+g.addEdge("E","F");
+
+// g.depthFirstRecursive("A");
+// g.depthFirstIterative("A");
+// g.breadthFirst("A");
